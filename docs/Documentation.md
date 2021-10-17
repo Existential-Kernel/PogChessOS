f44b9004e570045c0cef88db17c4637d80ea75ea
nonce <77142078+Existential-nonce@users.noreply.github.com>
2021-10-17 01:50:52 +0100

    Engine and doc improvements

---
+++ docs/Documentation.md
@@ -0,0 +1,30 @@
+<h1 align="center">Year 12 programming project documentation</h1>
+
+## Table of contents
+
+* idk
+* idk
+* idk
+* idk
+
+---
+
+# Introduction
+    ## Planning
+        - why i chose a chess engine as my project
+        - GUI or CLI decision
+        - language choice
+
+# How the engine works
+    ## Fundamental functionality
+    ## Fundamental algorithm
+
+    ## Engine code
+
+    ## CLI code
+        - ncurses.h
+
+# Flowchart
+
+
+# Conclusion
--- engine/mainframe.c
+++ engine/mainframe.c
@@ -2,25 +2,24 @@
 #include "defs.h"
 #include "stdlib.h"
 
-#define FEN1 "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
-#define FEN2 "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-#define FEN3 "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
-#define FEN4 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
 
 int main() {	
 
 	AllInit();	
-
-	S_BOARD board[1];
 	
-	ParseFen(FEN4, board);
+	int move = 0;
+	int from = A2; int to = H7;
+	int cap = wR; int prom = bK;
 	
-	PrintBoard(board);
+	move = ( ( from ) | ( to << 7 ) | ( cap << 14 ) | ( prom << 20) );
+			
+	printf("from:%d to:%d cap:%d prom:%d\n",
+		FROMSQ(move),TOSQ(move),CAPTURED(move),
+		PROMOTED(move));
 	
-	ASSERT(CheckBoard(board));
-	printf("\nForced asserts...\n");
-	board->posKey ^= SideKey;
-	ASSERT(CheckBoard(board)); 
+	printf("Algebraic from:%s\n",PrSq(from));
+	printf("Algebraic to:%s\n",PrSq(to));
+	printf("Algebraic move:%s\n",PrMove(move));
 	
 	
 	return 0;
